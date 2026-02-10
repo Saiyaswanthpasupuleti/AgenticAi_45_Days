30e04adb15c3a719500ef5d452f7793c1aaf95ed
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-02-10 12:40:00 +0530

    day3

---
+++ Day3/AI/AI/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from ollama import Client
+import json
+
+@csrf_exempt
+def chat(request):
+    client = Client(
+        host="http://localhost:11434"
+    )
+    if request.method == 'POST':
+        bodyMessage = json.loads(request.body)
+        print(bodyMessage)
+        message_content = bodyMessage.get('message') 
+        stream = client.chat(
+            model="gemma3:270m",
+            messages=[{"role": "user", "content": message_content}]
+        )
+        return HttpResponse(stream.message.content)
